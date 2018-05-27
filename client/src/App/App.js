dab00897a61c237169403b5191ae8404dc45c2f0
Dtaitt <donovan.taitt@yahoo.com>
2018-05-27 18:06:32 -0500

    created Header

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,9 +1,15 @@
+//@flow
 import React, { Component } from 'react';
 import axios from 'axios';
 import logo from './../logo.svg';
 import './App.css';
 
-class App extends Component {
+import Header from './../Header/Header';
+
+type Props = {};
+type State = {};
+
+class App extends Component<Props, State> {
 
   async componentDidMount() {
     const res = await axios.get('/favorites/?format=json')
@@ -13,13 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
       </div>
     );
   }
---
+++ client/src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+import { Navbar } from 'react-materialize';
+
+export default function Header() {
+    return (
+        <header>
+            <div className="my-container">
+                <Navbar brand='Font Finder' />
+            </div>
+        </header>
+    )
+}
