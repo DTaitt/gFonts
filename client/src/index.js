0204381e9c003ec7da8ad755f822a6cef5265d30
Dtaitt <donovan.taitt@yahoo.com>
2018-05-27 18:35:00 -0500

    created redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {
+    Provider
+} from 'react-redux'
+import {
+    createStore
+} from 'redux'
+import rootReducer from './redux/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App /> 
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
