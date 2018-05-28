75ebfec18f7d5947d75da983ba07e4570122dc37
Dtaitt <donovan.taitt@yahoo.com>
2018-05-28 12:03:20 -0500

    created Main

---
+++ client/src/Main/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Main.css';
+
+// import CardDisplayContainer from './../CardDisplayContainer/CardDisplayContainer';
+// import Sidebar from './../Sidebar/Sidebar';
+
+type Props = {}
+
+export default function Main(props: Props) {
+    return (
+        <main>
+            <div className="my-container">
+                {/* <CardDisplayContainer />
+                <Sidebar /> */}
+            </div>
+        </main>
+    )
+}
