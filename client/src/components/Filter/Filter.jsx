800f58b4b4d2f553ba2ec3121efbf198fce7cba8
Dtaitt <donovan.taitt@yahoo.com>
2018-06-03 12:35:49 -0500

    created Filter

---
+++ client/src/components/Filter/Filter.jsx
@@ -0,0 +1,61 @@
+// @flow
+import React from 'react';
+import './Filter.css';
+
+import {Row, Input, Collection, CollectionItem} from 'react-materialize';
+
+type Props = {
+  handleSearch(e:any):void,
+  handleCategory(e:any):void,
+  handleVariant(e:any):void,
+  categoryOptions: string[],
+  variantOptions: Object[],
+};
+
+export default function Filter(props:Props) {
+    return (
+        <div className="filter">
+            <Collection>
+                <CollectionItem className="search">
+                    <Row>
+                        <Input s={6} label="Search" validate /*onChange={props.handleSearch}*/ />
+                    </Row>
+                </CollectionItem>
+                {/* <CollectionItem className="Categories">
+                    <Row>
+                        <Input 
+                            s={12} 
+                            type="select" 
+                            label="Category Type" 
+                            defaultValue="0" 
+                            onChange={props.handleCategory}
+                        >
+                            {
+                                props.categoryOptions.map((option) => {
+                                    return <option key={option} value={option}>{option}</option>
+                                })
+                            }
+                        </Input>
+                    </Row>
+                </CollectionItem> */}
+                {/* <CollectionItem className='variant'>
+                    <Row onChange={props.handleVariant}>
+                        {
+                            props.variantOptions.map((variant) => {
+                                return (
+                                    <Input 
+                                        key={variant.value} 
+                                        name={variant.value} 
+                                        type="checkbox" 
+                                        value={variant.value} 
+                                        label={variant.label} 
+                                    />
+                                )
+                            })
+                        }
+                    </Row>
+                </CollectionItem>  */}
+            </Collection>
+        </div>
+    )
+}
--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Collapsible, CollapsibleItem, Card, Badge } from 'react-materialize';
 import './Sidebar.css';
+import Filter from '../Filter/Filter';
 
 type Props = {}
 
@@ -10,7 +11,7 @@ export default function Sidebar(props:Props){
         <Card className='sidebar cyan darken-2'>
             <Collapsible accordion>
                 <CollapsibleItem header='Filter' icon='search'>
-                    <p>FilterContainer</p>
+                    <Filter />
                 </CollapsibleItem>
                 <Badge>Num of Favs</Badge>
                 <CollapsibleItem header='Favorites' icon='favorite' className='favorite'>
