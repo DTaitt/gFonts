c9030d8389c0c66825c76394ec677f8bc3fd3b74
Dtaitt <donovan.taitt@yahoo.com>
2018-06-03 13:22:30 -0500

    added render tests for Filter

---
+++ client/src/components/Filter/Filter.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+enzyme.configure({ adapter: new Adapter() });
+
+import Filter from './Filter';
+import Search from '../Search/Search';
+import Categories from '../Categories/Categories';
+import Variants from '../Variants/Variants';
+
+const wrapper = shallow(<Filter />);
+
+describe("Filter", function () {
+    it("should render <Filter /> without throwing an error", function () {
+        expect(wrapper.exists(<div className="filter"></div>)).toBe(true);
+    });
+    it("should render <Search /> without throwing an error", function () {
+        expect(wrapper.exists(Search)).toBe(true);
+    });
+    it("should render <Categories /> without throwing an error", function () {
+        expect(wrapper.exists(Categories)).toBe(true);
+    });
+    it("should render <Variants /> without throwing an error", function () {
+        expect(wrapper.exists(Variants)).toBe(true);
+    });
+});
