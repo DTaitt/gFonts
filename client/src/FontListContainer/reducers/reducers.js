4fc61cc28947a49ec67a11c8c94a1d3ac5d04ab3
Dtaitt <donovan.taitt@yahoo.com>
2018-06-03 11:44:27 -0500

    typechecked FLC reducers

--- client/src/FontListContainer/reducers/reducers.js
+++ client/src/FontListContainer/reducers/reducers.js
@@ -1,4 +1,11 @@
-export function fontDataReducer(fontData = [], action) {
+//@flow
+
+type action = {
+    type:string,
+    payload:Object[],
+}
+
+export function fontDataReducer(fontData:Object[] = [], action:action):Object[] {
     switch (action.type) {
         case 'INITIALIZE_FONT_DATA':
             return [
