b815ca03fbe5fb1d798bef03a39b3976571e3d84
Dtaitt <Donovan.Taitt@yahoo.com>
2018-06-10 15:01:58 -0500

    tested FontList

---
+++ client/src/components/FontList/FontList.test.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import enzyme, {shallow, mount} from 'enzyme';
+
+import FontList from './FontList';
+import ConnectedFontContainer from 'components/FontContainer/FontContainer';
+import {formatFontUrl, setupWrapper} from 'utilities/utilities';
+import FontContainer from 'components/FontContainer/FontContainer';
+
+const initialProps = {
+	fontData: [
+		{
+			category:'category',
+			family:'family',
+			urlFamily:'urlFamily',
+			key:'family',
+			id:'family',
+			url:formatFontUrl('urlFamily'),
+			variants:[
+				'variant',
+				'second variant',
+				'third variant',
+			]
+		},
+		{
+			category:'category 2',
+			family:'family 2',
+			urlFamily:'urlFamily 2',
+			key:'family 2',
+			id:'family 2',
+			url:`${formatFontUrl('urlFamily')} 2`,
+			variants:[
+				'variant 2',
+				'second variant 2',
+				'third variant 2',
+			]
+		}
+	]
+};
+
+const {wrapper} = setupWrapper(<FontList {...initialProps} />, initialProps);
+const renderables = [
+	{
+		name: 'itself',
+		node: <section className="card-display" />
+	},
+	{
+		name: '<ConnectedFontContainer />',
+		node: <ConnectedFontContainer />
+	},
+];
+
+describe('FontList', () => {
+	describe('should render', () => {
+		renderables.forEach(renderable => {
+			it(renderable.name , () => {
+				expect(wrapper.exists(renderable.node)).toBe(true);
+			});
+		});
+	});
+});
