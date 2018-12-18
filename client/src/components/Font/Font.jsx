f6cf6757b9ca676d1a82262bdcf9efded25b3992
Donovan <Donovan.Taitt@yahoo.com>
2018-12-18 18:46:27 -0500

    added infinite scroller to aid in performance

--- client/src/components/Font/Font.jsx
+++ client/src/components/Font/Font.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { memo } from 'react';
 import { CardPanel, Button } from 'react-materialize';
 import { connect } from 'react-redux';
 import _pipe from 'lodash.flow'
@@ -8,30 +8,31 @@ import FontInfo from 'components/FontInfo/FontInfo';
 import { separateByPlus, createHtmlLink, createCssStyle, copyToClipboard } from 'utilities/utilities';
 import './Font.css'
 
-const Font = (props) => (
-	<CardPanel 
-		className="font white black-text z-depth-2"
-	>
-		<FontInfo 
-			category={props.category} 
-			family={props.family} 
-			id={props.id}
-			url={props.url}
-			variants={props.variants}
-		/>
-		<div className="font__interaction">
-			<div className="font__interaction__code">
-				<Button onClick={() => _pipe(separateByPlus, createHtmlLink, copyToClipboard)(props.family)}>Copy HTML</Button>
-				<Button onClick={() => _pipe(createCssStyle, copyToClipboard)(props.family)}>Copy CSS</Button>
-			</div>
-			<Button 
-				className='font__interaction__btn red' 
-				waves='light' 
-				floating
-				icon='favorite'
+const Font = memo((props) => (
+		<CardPanel 
+			className="font white black-text z-depth-2"
+		>
+			<FontInfo 
+				category={props.category} 
+				family={props.family} 
+				id={props.id}
+				url={props.url}
+				variants={props.variants}
 			/>
-		</div>
-	</CardPanel>
+			<div className="font__interaction">
+				<div className="font__interaction__code">
+					<Button onClick={() => _pipe(separateByPlus, createHtmlLink, copyToClipboard)(props.family)}>Copy HTML</Button>
+					<Button onClick={() => _pipe(createCssStyle, copyToClipboard)(props.family)}>Copy CSS</Button>
+				</div>
+				<Button 
+					className='font__interaction__btn red' 
+					waves='light' 
+					floating
+					icon='favorite'
+				/>
+			</div>
+		</CardPanel>
+	)
 )
 
 const mapDispatchToProps = ({ addFavorite });
--- client/src/components/FontList/FontList.jsx
+++ client/src/components/FontList/FontList.jsx
@@ -1,35 +1,58 @@
 //@flow
-import React, { useEffect } from 'react';
-import _pipe from 'lodash.flow';
+import React, { memo, useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import InfiniteScroll from 'react-infinite-scroller'
+import _pipe from 'lodash.flow'
 import { initializeFontData } from 'redux/state/fonts/actions';
-import { prefixWithUrlRoute, separateByPlus } from 'utilities/utilities';
 import Font from 'components/Font/Font';
+import { separateByPlus, prefixWithUrlRoute } from 'utilities/utilities'
 
 import './FontList.css'
 
-const FontList = (props) => {
+const FontList = memo((props) => {
+
+	useEffect(() => props.initializeFontData(), [])
+
+	const [ startIndex, setStartIndex ] = useState(0)
+
+	const [ fonts, setFonts ] = useState([])
+
+	const loadMoreFonts = (numOfFonts) => {
+		const endIndex = startIndex + numOfFonts;
+		const newFonts = props.fonts.slice(startIndex, endIndex)
+		setFonts([...fonts, ...newFonts])
+		setStartIndex(startIndex + numOfFonts)
+	}
+
 	useEffect(() => {
-		props.initializeFontData();
-	},[])
+		const hasNotLoadedAllFonts = fonts.length !== props.fonts.length
+		hasNotLoadedAllFonts && loadMoreFonts(12) 
+	}, [props.fonts])
 
 	return(
 		<section className="font-list">
-			{
-				props.fonts.map((font) => (
-					<Font
-						category={font.category}
-						family={font.family}
-						key={font.family}
-						id={font.family}
-						url={_pipe(separateByPlus, prefixWithUrlRoute)(font.family)}
-						variants={font.variants}
-					/>
-				))
-			}
+            <InfiniteScroll
+				pageStart={0}
+				loadMore={loadMoreFonts}
+				threshold={500}
+				hasMore={fonts.length !== props.fonts.length}
+                loader={null}>
+                {
+					fonts.map((font) => (
+						<Font
+							category={font.category}
+							family={font.family}
+							key={font.family}
+							id={font.family}
+							url={_pipe(separateByPlus, prefixWithUrlRoute)(font.family)}
+							variants={font.variants}
+						/>
+					))
+				}
+            </InfiniteScroll>
 		</section>
 	)
-}
+})
 
 const mapStateToProps = (state) => ({ fonts: state.filteredFonts })
 const mapDispatchToProps = ({ initializeFontData });
